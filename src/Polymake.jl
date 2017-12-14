308340a6c41e267389db3f31119fb3683d4065a6
William Hart <goodwillhart@googlemail.com>
2017-12-14 16:59:04 +0100

    Initial initialisation of Polymake.

--- src/Polymake.jl
+++ src/Polymake.jl
@@ -1,5 +1,31 @@
 module Polymake
 
-# package code goes here
+using Cxx
+
+const pkgdir = realpath(joinpath(dirname(@__FILE__), ".."))
+const libdir = joinpath(pkgdir, "local", "lib")
+
+prefix = joinpath(Pkg.dir("Polymake"), "local");
+
+addHeaderDir(joinpath(prefix, "include"), kind = C_User)
+
+const libpolymake = joinpath(pkgdir, "local", "lib", "libpolymake")
+
+function __init__()
+   push!(Libdl.DL_LOAD_PATH, libdir)
+
+   if is_linux()
+      Libdl.dlopen(libpolymake)
+   end
+
+   cxxinclude(joinpath("polymake/Main.h"), isAngled = false)
+end
+
+function init()
+   icxx"""using namespace polymake; Main* polymake_main = new Main();"""
+   #icxx"""using namespace polymake; greeting(2);"""
+end
+
+export init
 
 end # module
